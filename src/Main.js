e57dd791164d71220f8b821e4e2b4e86a5f3b125
NarendrAryasutaWijaya <narendra_aryasuta_29rpl@student.smktelkom-mlg.sch.id>
2022-03-24 06:58:46 +0700

    p

---
+++ src/Main.js
@@ -0,0 +1,22 @@
+import React from "react"
+import { Route, Switch } from "react-router-dom";
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Contact from "./pages/Contact"
+import Gallery from "./pages/Gallery"
+import Cart from "./pages/Cart"
+ 
+class Main extends React.Component{
+    render(){
+        return (
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/about" component={About} />
+                <Route path="/contact" component={Contact} />
+                <Route path="/gallery" component={Gallery} />
+                <Route path="/Cart" component={Cart} />
+            </Switch>
+        )
+    }
+}
+export default Main;
